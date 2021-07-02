6c6a4760132c3037c59548a0d0bbed165161b76f
Brandon <43820089+BManNap@users.noreply.github.com>
2021-07-02 14:50:18 -0500

    Add files via upload

---
+++ back.js
@@ -0,0 +1,9 @@
+function logging(id) {
+   // body...
+   document.getElementById("hey").innerHTML += id;
+   console.log(id)
+}
+function reset() {
+   // body...
+   document.getElementById("hey").innerHTML = ""
+}
